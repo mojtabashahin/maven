90fe184cb54134a2aee9a0aecaa24c0247c75596
E116324 <E116324@m4rk7cgqx3.lan>
2025-01-17 15:34:11 +1100

    new commit

---
+++ maven-org/src/test/java/Mp3UtilTest.java
@@ -0,0 +1,17 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.nio.file.Paths;
+
+public class Mp3UtilTest {
+
+    @Test
+    void getArtist_shouldBe_KraftClub() {
+        String artist = Mp3Util.getArtist(Paths.get("./my.mp3"));
+        Assertions.assertEquals("Maaarrrcoooo", artist);
+    }
+}
